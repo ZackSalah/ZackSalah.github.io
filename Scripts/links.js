a96ba37da0a40424cadf0abe3d80c32fc0e79a3a
ZackSalah <ms24@pdx.edu>
2019-08-19 18:36:22 +0300

    fixed issues with navbar collapse

--- Scripts/links.js
+++ Scripts/links.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.getElementById("gitpages").onclick = function() {
-	window.open('http://bit.ly/KratosApp');
+	window.open('http://bit.ly/MS73KratosApp');
 };
 
 document.getElementById("contactForm").setAttribute("action", "//formspree.io/" + "msalahcs73" + "+" + "PortfolioSite" + "@" + "gmail"+ "." + "com");
---
+++ Scripts/scroll.js
@@ -0,0 +1,36 @@
+window.addEventListener("hashchange", function() {
+	console.log(event.oldURL)
+	console.log(event.newURL)
+
+	if (window.location.hash !== "#contact" ){
+		scrollBy(0, -63)
+	}
+})
+
+// function checkHash(clickedHashName){
+// 	if(clickedHashName === window.location.hash){
+// 		window.history.back();
+// 	}
+// }
+const cells = document.getElementsByClassName("nav-link")
+for (let cell of cells) {
+	if(!window.HashChangeEvent){
+		cell.onclick = function() {
+	  		console.log(event.oldURL);
+	  		};
+	}
+}
+
+$( window ).resize(function() {
+	var hamburgerMenuStatus = document.querySelector("button").classList.contains("collapsed");
+	if(!hamburgerMenuStatus){
+	    $(".show").removeClass("show")
+	}
+	// $(".show").collapse("hide");
+});
+$(document).mouseup(function(e) 
+{
+	if($(".navbar-toggler").css('display') === "block"){
+	    $(".show").collapse("hide")
+	}
+});
